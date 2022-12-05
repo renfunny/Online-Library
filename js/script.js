e905d950c760f7340999b341caefbdedf0d50636
Glenroy <kendalbastien@gmail.com>
2022-12-05 18:17:25 -0500

    edit js code

--- js/script.js
+++ js/script.js
@@ -26,25 +26,6 @@ window.onscroll = () =>{
 
 }
 
-window.onload = () =>{
-
-  if(window.scrollY > 80){
-    document.querySelector('.header .header-2').classList.add('active');
-  }else{
-    document.querySelector('.header .header-2').classList.remove('active');
-  }
-
-  fadeOut();
-
-}
-
-function loader(){
-  document.querySelector('.loader-container').classList.add('active');
-}
-
-function fadeOut(){
-  setTimeout(loader, 4000);
-}
 
 var swiper = new Swiper(".books-slider", {
   loop:true,
