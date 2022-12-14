992c3f9df2ef2acda58cf4e928e616f4e7e07455
Renato Cordova <ren99cordova@gmail.com>
2022-12-14 18:33:15 -0500

    fixed problem where style and script was being lost when different pathways were fetched, added other routes to find books by id, title, author, and genre and fixed routes for images as well

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,7 +16,7 @@ router.get(`/`, async (req, res) => {
 });
 
 //books by genre
-router.get("/:genre", async (req, res) => {
+router.get("/genre/:genre", async (req, res) => {
   try {
     const booksData = await Book.findAll({
       where: {
@@ -35,20 +35,56 @@ router.get("/:genre", async (req, res) => {
   }
 });
 
-// //one book by id
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const bookData = await Book.findByPk(req.params.id);
-//     if (!bookData) {
-//       res.status(404).json({ message: "No book with this id!" });
-//       return;
-//     }
-//     const book = bookData.get({ plain: true });
-//     console.log(book);
-//     res.render(`book`, book);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//one book by id
+router.get("/book/:id", async (req, res) => {
+  try {
+    const bookData = await Book.findByPk(req.params.id);
+    if (!bookData) {
+      res.status(404).json({ message: "No book with this id!" });
+      return;
+    }
+    const book = bookData.get({ plain: true });
+    console.log(book);
+    res.render(`book`, book);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//one book by title
+router.get("/title/:title", async (req, res) => {
+  try {
+    const bookData = await Book.findOne({
+      where: {
+        title: req.params.title,
+      },
+    });
+    const book = bookData.get({ plain: true });
+    console.log(book);
+    res.render(`book`, book);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// books by author
+router.get("/author/:author", async (req, res) => {
+  try {
+    const booksData = await Book.findAll({
+      where: {
+        author: req.params.author,
+      },
+    });
+
+    const books = booksData.map((book) => book.get({ plain: true }));
+    console.log(booksData);
+
+    res.render("genre", {
+      books,
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = router;
